7d15d4201a9f6680cb463782d063539f5afc80d9
cuijianpeng <cuijianpeng@jodogame.com>
2016-08-03 20:46:29 +0800

    update

---
+++ ASLib-0.2.0/ios/ASLib.framework/Versions/A/Headers/test.h
@@ -0,0 +1,12 @@
+//
+//  test.h
+//  Pods
+//
+//  Created by cuijianpeng on 16/8/3.
+//
+//
+
+#import <Foundation/Foundation.h> 
+@interface test : NSObject
++(void)djconnect;
+@end
--- ASLib/Classes/test.m
+++ ASLib/Classes/test.m
@@ -10,8 +10,12 @@
 #import <CocoaAsyncSocket/GCDAsyncSocket.h>
 @implementation test
 +(void)djconnect{
+    NSLog(@"djtest--1");
     GCDAsyncSocket *tem = [GCDAsyncSocket new];
+    NSLog(@"djtest--2");
     NSError *err;
+    NSLog(@"djtest--3");
     [tem connectToHost:@"www.baidu.com" onPort:80 error:&err];
+    NSLog(@"djtest--4");
 }
 @end
